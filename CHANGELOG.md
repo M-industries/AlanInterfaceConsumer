9cc86faced876556d3b20d30b6eaf3731fa31f9a
Gerbert van Nieuwaal <gvannieuwaal@m-industries.com>
2018-05-23 11:18:44 +0200

    9.0.1 Add README; Explaining handling large initial interface data

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# Changelog
+
+## 9.0.0
+Initial version
+
+## 9.0.1
+Add README
+Explaining handling large initial interface data
--- build.sh
+++ build.sh
@@ -12,4 +12,5 @@ rm -fr                        build/npm
 cp -r build/js                build/npm
 cp    src/LICENSE             build/npm
 cp    src/package.json        build/npm
+cp    README.md               build/npm
 cp -r src/lib/stream_handler  build/npm/lib/stream_handler
